f63cfaa0937d43872bcd5d95a3ad2b76efb7e59b
郑乃中 <zhengnaizhong@gmail.com>
2020-09-15 23:24:03 +0800

    Initial commit

---
+++ src/main/java/com/cw/outsourcing/outsourcingbackend/OutsourcingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cw.outsourcing.outsourcingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutsourcingBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OutsourcingBackendApplication.class, args);
+    }
+
+}
