d052a99b1fb57ce0ee65011b6a99cdb4261bb4ed
郑乃中 <zhengnaizhong@gmail.com>
2020-09-22 22:56:19 +0800

    feat(redis): 增加redis序列化配置

---
+++ src/main/java/com/cw/outsourcing/outsourcingbackend/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package com.cw.outsourcing.outsourcingbackend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(RedisSerializer.string());
+        template.setHashKeySerializer(RedisSerializer.string());
+        template.setValueSerializer(RedisSerializer.json());
+        template.setHashValueSerializer(RedisSerializer.json());
+        return template;
+    }
+
+}
